13bcd06de312ba27202fd3fd65df1884a7e5cbc1
Manoj Kumar <162701324+ManojCodeCraft@users.noreply.github.com>
2024-07-21 17:28:17 +0000

    math and num

---
+++ Basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+// const score=400
+// const balance=new Number(100);
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber=1123.8988
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++ Maths ++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(3,2,5,6,8));
+// console.log(Math.max(3,2,5,6,8));
+
+
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
+
+
+
+
+
+
